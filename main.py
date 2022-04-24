7533a642a57aa29882128efaa201c71e87ded9fa
AiBatwa <ah444mad@hotmail.com>
2022-04-24 19:00:49 +0300

    Added support for the program to deal with 3 modes of operation. The program now recognizes Aruco with id 200 and 201. Each one of them is for a different mode. Also added an enum (Mode) to define the modes. Jumping between modes is handled currently through pressing p and f.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import time
+import enum
 import imutils
 import math
 import depthai as dai
-# from pynput import keyboard
 import numpy as np
 import cv2
 from cv2 import aruco
 from TCPLink import TCP_init, send, receive
 from OAKdLink import oakd_init, set_oakd_props, aruco_init
 from Controller import aruco_control, frame_counter, max_x
+
+
+class Mode(enum.Enum):
+    follow = 1
+    push = 2
+    remote = 3
 
 
 def main():
@@ -28,6 +34,9 @@ def main():
     print("[INFO] Setting up Aruco dictionary and camera coefficients ...")
     camera_matrix, distortion_coeffs, arucoDict, arucoParams = aruco_init()
 
+    # Set default mode
+    mode = Mode.push
+
     time.sleep(2.0)  # Necessary !!!
 
     print("[INFO] Initializing TCP connection ...")
@@ -40,8 +49,8 @@ def main():
         video = device.getOutputQueue(name="video", maxSize=1, blocking=False)  # establish queue
         # start = timeit.default_timer()  # Enable for debugging
         while True:
-            # with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-            #     pass
+            if mode is Mode.remote:  # TODO: Stop TCP Connection
+                continue
 
             videoIn = video.get()  # OAK-D cam
             frame = videoIn.getCvFrame()  # OAK-D cam
@@ -55,16 +64,24 @@ def main():
                                                                distCoeff=distortion_coeffs)
             tx, ty, tz, norm_x = np.Inf, np.Inf, np.Inf, np.Inf  # initializing position values
             rx, ry, rz = np.Inf, np.Inf, np.Inf  # initializing rotation values
-            # draw borders
-            if len(corners) > 0:  # add remote control boolean
+
+            # Cases in which you do nothing
+            if (len(corners) != 1  # If more than one Aruco detected (or non)
+                    or (mode is Mode.push and ids != 201)  # If robot in pushing mode, but it reads follow Aruco
+                    or (mode is Mode.follow and ids != 200)):  # If robot in follow mode, but it reads push Aruco
+                fc = frame_counter(fc, inc=False, dec=True)
+                if fc == 0:
+                    speed, steer = 0, 0
+
+            elif len(corners) == 1:  # Exactly one Aruco detected
                 fc = frame_counter(fc, inc=True, dec=False)
 
+                # Following mode: 15.8cm.   Pushing mode: 3.5cm
+                aruco_size = 0.158 if np.squeeze(ids) == 200 else 0.035
                 # Get the rotation and translation vectors
                 rvecs, tvecs, obj_points = cv2.aruco.estimatePoseSingleMarkers(
                     corners,
-                    0.258,
-                    # 0.053,
-                    # 0.07,
+                    aruco_size,
                     camera_matrix,
                     distortion_coeffs)
 
@@ -80,60 +97,63 @@ def main():
 
                 rvecs = np.squeeze(rvecs)
                 rx, ry, rz = obtain_angles(rvecs)
-                
+
                 speed, steer = aruco_control(tz, 400, 90, 60, norm_x)
 
-                # print("distances: [x: {:.2f},".format(tx), "y: {:.2f}, ".format(ty), "z: {:.2f}] ".format(tz),
-                #       ", norm x position: {:.3f}".format(tx / maximum_x * 10))  # Why multiply by 10?
-                # Add text to the image
-            else:
-                fc = frame_counter(fc, inc=False, dec=True)
-                if fc == 0:
-                    speed, steer = 0, 0
-
-            # Resize the frame (This is safe, because we already did the processing)
-            frame = imutils.resize(frame, 800)
-
-            # Enable for debugging
-            frame = cv2.flip(frame, 1)
-
-            # Put text on the frame to display it
-            cv2.putText(frame, "Position: x:%.2f, y:%.2f, z:%.2f, x_percentage:%3f" % (tx, ty, tz, norm_x),
-                        (0, 100), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
-                        color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
-            if rz <0: rz+=360
-            cv2.putText(frame, "Rotation: x:%.2f, y:%.2f, z:%.2f" % (rx,ry,rz),
-                        (0, 200), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
-                        color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
-
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+            key = show_frame(frame, tx, ty, tz, norm_x, rx, ry, rz, aruco_id=ids if len(corners) == 1 else math.inf)
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                 break
+            if key == ord("p"):
+                mode = Mode.push
+            if key == ord("f"):
+                mode = Mode.follow
 
             # send(s, speed, steer)
-            # receive(s, debug=True)
+            # receive(s, debug=False)
 
-        # listener.join
-        
+
+def show_frame(frame, tx=math.inf, ty=math.inf, tz=math.inf, norm_x=math.inf, rx=math.inf, ry=math.inf, rz=math.inf,
+               aruco_id=math.inf):
+    # Resize the frame (This is safe, because we already did the processing)
+    frame = imutils.resize(frame, 800)
+
+    # Enable for debugging
+    frame = cv2.flip(frame, 1)
+
+    # Put text on the frame to display it
+    cv2.putText(frame, "Position: x:%.2f, y:%.2f, z:%.2f, x_percentage:%3f" % (tx, ty, tz, norm_x),
+                (0, 100), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+    if rz < 0: rz += 360
+    cv2.putText(frame, "Rotation: x:%.2f, y:%.2f, z:%.2f" % (rx, ry, rz),
+                (0, 200), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.putText(frame, "ID: %.2f" % aruco_id,
+                (0, 300), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                color=(0, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    return key
+
+
 def obtain_angles(rvec):
     # https://github.com/tizianofiorenzani/how_do_drones_work/blob/master/opencv/aruco_pose_estimation.py
-    #-- Obtain the rotation matrix tag->camera
-    R_ct    = np.matrix(cv2.Rodrigues(rvec)[0])
-    R_tc    = R_ct.T
-    R_flip  = np.zeros((3,3), dtype=np.float32)
-    R_flip[0,0] = 1.0
-    R_flip[1,1] =-1.0
-    R_flip[2,2] =-1.0
+    # -- Obtain the rotation matrix tag->camera
+    R_ct = np.matrix(cv2.Rodrigues(rvec)[0])
+    R_tc = R_ct.T
+    R_flip = np.zeros((3, 3), dtype=np.float32)
+    R_flip[0, 0] = 1.0
+    R_flip[1, 1] = -1.0
+    R_flip[2, 2] = -1.0
 
-    #-- Get the attitude in terms of euler 321 (Needs to be flipped first)
-    roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip*R_tc)
+    # -- Get the attitude in terms of euler 321 (Needs to be flipped first)
+    roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_flip * R_tc)
 
-    #-- Print the marker's attitude respect to camera frame
-    str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),math.degrees(pitch_marker),
-                        math.degrees(yaw_marker))
-    return math.degrees(roll_marker),math.degrees(pitch_marker), math.degrees(yaw_marker)
+    # -- Print the marker's attitude respect to camera frame
+    str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f" % (math.degrees(roll_marker), math.degrees(pitch_marker),
+                                                                  math.degrees(yaw_marker))
+    return math.degrees(roll_marker), math.degrees(pitch_marker), math.degrees(yaw_marker)
 
 
 # Checks if a matrix is a valid rotation matrix.
@@ -145,9 +165,9 @@ def isRotationMatrix(R):
     return n < 1e-6
 
 
-# Calculates rotation matrix to euler angles
+# Calculates rotation matrix to euler angles100big
 # The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
+# of the euler angles100big ( x and z are swapped ).
 def rotationMatrixToEulerAngles(R):
     assert (isRotationMatrix(R))
 
@@ -165,7 +185,6 @@ def rotationMatrixToEulerAngles(R):
         z = 0
 
     return np.array([x, y, z])
-
 
 
 if __name__ == '__main__':
